8b143a03a0534d9c27a1144e9bc5d0c9b766e540
matheusmessora <matheus.messora.vpn@gmail.com>
2023-04-03 18:51:59 -0300

    first endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+let usuarios = 0
+
 router.get('/', (req, res) => {
   const a = 1
   const b = 2
   const soma = a + b
-  res.json({ sum: soma})
+  usuarios++
+  res.json({ sum: soma, usuarios: usuarios})
 });
 
 module.exports = router;
