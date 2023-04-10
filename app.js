a3dd876aa8070060c17c215c8013b215fe554dcf
matheusmessora <matheus.messora.vpn@gmail.com>
2023-04-10 17:03:50 -0300

    CRUD Participantes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require('./routes/index');
 
 const app = express();
+
+app.use(express.json())
 app.use('/', routes);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,56 @@
+const {PrismaClient} = require('@prisma/client')
 const express = require('express');
 
 const router = express.Router();
+const prisma = new PrismaClient()
 
-let usuarios = 0
+router.get('/participantes', async (req, res) => {
+    await prisma.$connect()
+    const all = await prisma.participantes.findMany();
 
-router.get('/', (req, res) => {
-  const a = 1
-  const b = 2
-  const soma = a + b
-  usuarios++
-  res.json({ sum: soma, usuarios: usuarios})
+    // IMPLEMENTAR o NOT FOUND
+    res.status(200).json(all);
+})
+
+router.post('/participantes', async (req, res) => {
+    let {name, github_url} = req.body
+    const data = {
+        name: name,
+        github_url: github_url};
+    // VALIDACAO DE CAMPOS
+    await prisma.$connect()
+    const user = await prisma.participantes.create({
+      data: data,
+    });
+
+    res.status(201).json(user);
+});
+
+router.put('/participantes/:id', async (req, res) => {
+    const id = req.params.id;
+    let {name, github_url} = req.body
+
+    await prisma.$connect()
+    const user = await prisma.participantes.findUnique({
+      where: {
+        id: id,
+      },
+    })
+    // IMPLEMENTAR O NOT FOUND
+    const data = {
+        name: name,
+        github_url: github_url,
+        updated: new Date()
+    }
+
+    const updatedUser = await prisma.participantes.update({
+      where: {
+        id: user.id,
+      },
+      data: data,
+    })
+
+    res.status(200).json(updatedUser);
 });
 
 module.exports = router;
